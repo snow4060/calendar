0f655819023a6745f1c50dfe52ab9cb5e3d38b11
snow4060 <luohaoyun4068@students.esuhsd.org>
2023-09-04 20:17:26 -0700

    yuh

---
+++ main.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const {app, BrowserWindow, Menu} = require("electron");
+
+const isDev = process.env.NODE_ENV !== 'production';
+
+function createMainWindow(){
+    const mainWindow = new BrowserWindow({
+        title: "Calendar",
+        autoHideMenuBar: "true"
+    });
+    mainWindow.maximize();
+
+    if(isDev){
+        mainWindow.webContents.openDevTools();
+    }
+
+    mainWindow.loadFile(path.join(__dirname, './renderer/index.html'));
+}
+
+app.whenReady().then(() => {
+    createMainWindow();
+
+    // const mainMenu = Menu.buildFromTemplate(menu);
+    // Menu.setApplicationMenu(mainMenu);
+
+    app.on('activate', () => {
+        if(BrowserWindow.getAllWindows().length === 0){
+            createMainWindow();
+        }
+    });
+});
+
+const menu = [
+    {
+        label: 'File',
+        submenu: {
+            label: 'Quit',
+            click: () => app.quit(),
+            accelerator: 'CmdOrCtrl+W'
+        }
+    }
+];
+
+app.on('window-all-closed', () => {
+    if(process.platform !== 'darwin'){
+        app.quit();
+    }
+});
